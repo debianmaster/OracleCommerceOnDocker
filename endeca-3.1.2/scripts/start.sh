7cd807aed867ec8571c095a456ee76fa8186995a
Dynamo Stack <9chakri+ds@gmail.com>
2016-09-09 01:25:59 -0400

    my changes

--- endeca-3.1.2/scripts/start.sh
+++ endeca-3.1.2/scripts/start.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
 ENDECA_LOCAL_ROOT="/appl/endeca"
-$ENDECA_LOCAL_ROOT/PlatformServices/6.1.3/tools/server/bin/startup.sh
-$ENDECA_LOCAL_ROOT/CAS/3.1.1/bin/cas-service.sh &
-$ENDECA_LOCAL_ROOT/ToolsAndFrameworks/3.1.1/server/bin/startup.sh
+$ENDECA_LOCAL_ROOT/PlatformServices/6.1.4/tools/server/bin/startup.sh
+$ENDECA_LOCAL_ROOT/CAS/3.1.2.1/bin/cas-service.sh &
+$ENDECA_LOCAL_ROOT/ToolsAndFrameworks/3.1.2/server/bin/startup.sh
+
+while :; do
+  sleep 300
+done
+
+#ENDECA_LOCAL_ROOT="/appl/endeca"
+#$ENDECA_LOCAL_ROOT/PlatformServices/6.1.3/tools/server/bin/startup.sh
+#$ENDECA_LOCAL_ROOT/CAS/3.1.1/bin/cas-service.sh &
+#$ENDECA_LOCAL_ROOT/ToolsAndFrameworks/3.1.1/server/bin/startup.sh
